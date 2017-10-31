7d14e9e2df8d8837e336596491a40d4ffc34b661
lujunwei <lujunwei@GSL-YANFA-019.AladingBank.com>
2017-10-31 13:33:46 +0800

    更改了xml中的配置

---
+++ maopojo/src/io/vixp/xtjyie/core/pojo/User.java
@@ -0,0 +1,46 @@
+package io.vixp.xtjyie.core.pojo;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name="user")
+public class User implements Serializable {
+    private String password;
+
+    private String username;
+
+    private static final long serialVersionUID = 1L;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", password=").append(password);
+        sb.append(", username=").append(username);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
