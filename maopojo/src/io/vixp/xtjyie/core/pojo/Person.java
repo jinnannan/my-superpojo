c5598585220018fc9e828eeadc8abad43b586d83
jinnannan <jinnannanbh@163.com>
2017-12-18 13:45:57 +0800

    person添加了一个字段password

--- maopojo/src/io/vixp/xtjyie/core/pojo/Person.java
+++ maopojo/src/io/vixp/xtjyie/core/pojo/Person.java
@@ -10,6 +10,16 @@ public class Person {
 	private Integer id;
 	
 	private String username;
+	
+	private String password;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 	public String getUsername() {
 		return username;
