477f3decf842cab2c6b745f2665ccba1979d2428
jinnannan <jinnannanbh@163.com>
2017-12-18 10:51:07 +0800

    添加了person类

---
+++ maopojo/src/io/vixp/xtjyie/core/pojo/Person.java
@@ -0,0 +1,19 @@
+package io.vixp.xtjyie.core.pojo;
+
+/**
+ * person类
+ * @author jinnannan
+ *
+ */
+public class Person {
+
+	private Integer id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+}
