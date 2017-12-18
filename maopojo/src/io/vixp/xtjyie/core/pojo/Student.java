619fdf6a43328f6f5ecfc4a98f3ba0fccf5e1555
jinnannan <jinnannanbh@163.com>
2017-12-18 10:23:33 +0800

    添加了一个student实体类

---
+++ maopojo/src/io/vixp/xtjyie/core/pojo/Student.java
@@ -0,0 +1,42 @@
+package io.vixp.xtjyie.core.pojo;
+
+/**
+ * 
+ * 学生实体类
+ * @author jinnannan
+ *
+ */
+public class Student {
+
+	private Integer id;//id
+	
+	private String username;//姓名
+	
+	private String password;//密码
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
